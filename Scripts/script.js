2bd2f858986760d729d86a91e1c3d1ad54254f7f
Jennifer Amanze <jennfranc18@gmail.com>
2025-07-20 21:27:59 +0100

    Updates
    Changes were made to the codebase, and a json package was added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,35 @@
 // Waitlist section interactivity
 
+async function sendEmail(email) {
+  try {
+    const response = await fetch('https://connect.mailerlite.com/api/subscribers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNTU4OWU5YmZlMmRiNTViMjA0NGNlZDYxN2QzZDhkNjdmODY2NjdjZDAyNmMzMTA2NTUwZDUyNjEwMjdjMzU3Y2M1MDk4MDFhN2M5ZTc2YWUiLCJpYXQiOjE3NTI5NDkxMzcuODU1MDQyLCJuYmYiOjE3NTI5NDkxMzcuODU1MDQ0LCJleHAiOjQ5MDg2MjI3MzcuODUxMDc2LCJzdWIiOiIxNjkwODY3Iiwic2NvcGVzIjpbXX0.YZ1EhXPQjgolbElDvJyQhBA4XqYFyDBGEOo9Dqq_-VJSjyvkQ6d5ya4N0r5Evl5qLOfV5lZbDjBjNhon2-3x-8GnyY3VJrrskdj5qBPnXz8xBEqFlsiCdUGdlimMqEzZ7f5D1Xt6DoVBJcOCWeYU0JKjgWVg5qKsTaM9wIf9Enhph94-V2e-1I43qzj1vW-21F1xxJ_KCUuAtLaL22RKMHlIZOSnWg-iX7UeEqHLbTMoijmdoNvTwWQyuVT5yILMlSoyAF4ccgv-Qh5HpzAa-CcDUT5sYSevI40pPAarhVZjxXzEF0NIFdmiPxBt5Cjpu0I5POqoadGDhxE4qj4JCkCOPOxDXoWd2-auwYxz2yXJGjQF0loOkqlDYe_5i4kuV6jSDn6ZeQrC_CVgvWPq0-i0z7MdOaHCP9plghDxY-3AXBBp5Xva9g3ZFTqMWKITsNyuFr3XaZkuUwsfKPeOzXIHLr0rH3WoSWyH2bUNJV6vPp8-y1teGUxe9rUJCp9ZHwOEYC8IdthBZbi_G_F9nZ3CV_bpsRQWpUjvQGCV4A6Xg3DT20fTQ-KgJCTy7eN6_uPqdqXl5Mzs146LE18iHF6KmLy6F5LSWshI42n-m6fFaw5wfiLm7QBwUhBllgddoSUf0EHgB8FRrfJ4iFG_CGvqz73I3OgTX3NXsfWhsLY',
+      },
+      body: JSON.stringify({
+        email,
+        name: 'Forextainment Waitlist',
+        groups: ['160378852328081233']
+      }),
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      console.log("Added to waitlist:", result);
+      // alert("You're successfully added to the waitlist!");
+    } else {
+      console.error("MailerLite Error:", result);
+      alert("There was an issue. Please try again.");
+    }
+  } catch (error) {
+    console.error("Network Error:", error);
+    alert("Connection issue. Please try again later.");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("waitlist-form");
   const emailInput = document.getElementById("waitlist-email");
@@ -50,6 +80,10 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
     if (!emailInput.value) return;
     submitBtn.disabled = true;
+   console.log("Submitting email:", emailInput.value);
+    const email = emailInput.value;
+    sendEmail(email)
+
     setStep(2);
 
     setTimeout(() => {
